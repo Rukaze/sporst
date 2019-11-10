77aaf6e0a435e703275413cbe7420787c855d219
EC2 Default User <ec2-user@ip-172-31-36-119.ap-northeast-1.compute.internal>
2019-11-10 13:03:04 +0000

    css only

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
    
   root "static_pages#home"
   get "about" => "static_pages#about"
-
+  get "baseball" => "static_pages#baseball"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'application#hello'
+  
+  
 end
