04a900cf10d55910e7f840edc8a660957f783ec3
EC2 Default User <ec2-user@ip-172-31-36-119.ap-northeast-1.compute.internal>
2019-11-17 05:16:32 +0000

    fin home

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   get "select_sports" => "posts#select_sports"
   get "new_content/:kind" => "posts#new" 
+  
   get "sports/:kind" => "posts#league"
   get ":kind/:league/posts/:id/show" => "posts#show"
   get "/:kind/:league" => "posts#index"
+  
   
   post "/create" => "posts#create" 
    
