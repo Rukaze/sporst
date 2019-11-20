e2588d54f17d34fe3113f9b53058f6ccac3d793c
EC2 Default User <ec2-user@ip-172-31-36-119.ap-northeast-1.compute.internal>
2019-11-20 14:00:46 +0000

    karate

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,7 +9,8 @@
 [ ['basketball', 'NBA'],
   ['soccer', 'Laliga'],
   ['baseball', 'NPB'],
-  ['baseball', 'MLB']
+  ['baseball', 'MLB'],
+  ['その他スポーツ', '空手']
 ].each do |kind, league|
   Sport.create!(
     { kind: kind, league: league }
