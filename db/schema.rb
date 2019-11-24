2887cb031091897a5efc9eee3e01c7d598a03023
EC2 Default User <ec2-user@ip-172-31-36-119.ap-northeast-1.compute.internal>
2019-11-24 08:01:05 +0000

    fin deluser

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20191115233915) do
+ActiveRecord::Schema.define(version: 20191124075431) do
 
   create_table "posts", force: :cascade do |t|
     t.string "kind"
