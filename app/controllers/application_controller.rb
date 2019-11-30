2722037fb9f1cc4f2151be4f6c7420fb2ba80125
EC2 Default User <ec2-user@ip-172-31-36-119.ap-northeast-1.compute.internal>
2019-11-30 11:19:18 +0000

    html

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action:set_current_user
   
-  def hello
-    render html: "hello"
+  def set_current_user
+    @current_user = current_user
   end
 end
