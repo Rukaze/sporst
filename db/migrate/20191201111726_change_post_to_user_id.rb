84528b535e1fd22043572da325500f34599b4cfa
EC2 Default User <ec2-user@ip-172-31-36-119.ap-northeast-1.compute.internal>
2019-12-01 11:36:13 +0000

    edit baria

--- db/migrate/20191201111726_change_post_to_user_id.rb
+++ db/migrate/20191201111726_change_post_to_user_id.rb
@@ -1,5 +1,12 @@
 class ChangePostToUserId < ActiveRecord::Migration[5.2]
   def change
-    change_column :posts, :user_id, :integer
+    if Rails.env.development? || Rails.env.test?
+      change_column :posts, :user_id, :integer
+    else Rails.env.production?
+      # 本番環境はusingオプションを追加
+      change_column :posts, :user_id, 'integer USING CAST(user_id AS integer)'
+    end
   end
+  
+  
 end
