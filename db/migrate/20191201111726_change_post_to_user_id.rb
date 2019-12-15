76866c74a9d1c2faa92870e83af910eea6dd2b50
EC2 Default User <ec2-user@ip-172-31-36-119.ap-northeast-1.compute.internal>
2019-12-15 10:08:29 +0000

    integer

--- db/migrate/20191201111726_change_post_to_user_id.rb
+++ db/migrate/20191201111726_change_post_to_user_id.rb
@@ -4,7 +4,8 @@ class ChangePostToUserId < ActiveRecord::Migration[5.2]
       change_column :posts, :user_id, :integer
     else Rails.env.production?
       # 本番環境はusingオプションを追加
-      change_column :posts, :user_id, 'integer USING CAST(user_id AS integer)'
+      change_column :posts, :user_id, :integer , limit: 2
+      #USING CAST(user_id AS integer)'
     end
   end
   
